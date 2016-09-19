60031b3641024044d6d4ce5f13452110a729d22d
Warwick Booth <warwick@lsdev.biz>
2016-09-19 11:05:15 +0200

    no message

--- classes/class-{plugin-name}-admin.php
+++ classes/class-{plugin-name}-admin.php
@@ -1,221 +1,42 @@
 <?php
 /**
- * LSX Currency Main Class
+ * {plugin-label} Main Class
  */
-class LSX_Currency_Admin extends LSX_Currency{	
+
+class {class-name}_Admin extends {class-name}{	
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
-		$this->set_defaults();
 		add_action('lsx_framework_dashboard_tab_content',array($this,'settings'),11);
-		add_action('lsx_framework_dashboard_tab_bottom',array($this,'settings_scripts'),11);
-		
-		add_filter('lsx_price_field_pattern',array($this,'fields'),10,1);
 	}
 	/**
-	 * outputs the dashboard tabs settings
+	 * Outputs the dashboard tabs settings
 	 */
 	public function settings() {
-	?>	
-		<tr class="form-field banner-wrap">
-			<th class="table_heading" style="padding-bottom:0px;" scope="row" colspan="2">
-			<label><h3 style="margin-bottom:0px;"><?php _e('Currency Settings',$this->plugin_slug); ?></h3></label>			
-			</th>
-		</tr>
-		<tr class="form-field">
-			<th scope="row">
-				<label for="openexchange_api">Open Exchange Rate API Key</label>
-			</th>
-			<td>
-				<input type="text" {{#if openexchange_api}} value="{{openexchange_api}}" {{/if}} name="openexchange_api" />
-				<br /><small><?php _e('Get your free API key here',$this->plugin_slug); ?> - <a target="_blank" href="https://openexchangerates.org/signup/free">openexchangerates.org</a></small>
-			</td>
-		</tr>			
-		<tr data-trigger="additional_currencies" class="lsx-select-trigger form-field-wrap">
-			<th scope="row">
-				<label for="currency"><?php _e('Base Currency',$this->plugin_slug);?></label>
-			</th>
-			<td>
-				<select value="{{currency}}" name="currency">
-					<?php
-					foreach($this->available_currencies as $currency_id => $currency_label ){ 
-
-						$selected = '';
-						if($currency_id === $this->base_currency){
-							$selected='selected="selected"';
-						}
-						echo '<option value="'.$currency_id.'" '.$selected.'>'.$currency_label.'</option>';
-					} ?>
-				</select>
-			</td>
-		</tr>
-		<tr data-trigger="currency" class="lsx-checkbox-action form-field-wrap">
-			<th scope="row">
-				<label for="modules"><?php _e('Additional Currencies',$this->plugin_slug);?></label>
-			</th>
-			<td><ul>
-			<?php 	
-			foreach($this->available_currencies as $slug => $label){
-				$checked = $hidden = '';
-				if(array_key_exists($slug,$this->additional_currencies) || $slug === $this->base_currency){
-					$checked='checked="checked"';
-				}
-				
-				if($slug === $this->base_currency){
-					$hidden = 'style="display:none;" class="hidden"';
-				}
-				?>
-				<li <?php echo $hidden; ?>>
-					<input type="checkbox" <?php echo $checked; ?> data-name="additional_currencies" data-value="<?php echo $slug; ?>" name="additional_currencies[<?php echo $slug; ?>]" /> <label for="additional_currencies"><?php echo $this->get_currency_flag($slug).$label; ?></label> 
-				</li>
-			<?php }
-			?>
-			</ul></td>
-		</tr> 
-		<tr class="form-field">
-			<th scope="row">
-				<label for="multi_price"><?php _e('Enable Multiple Prices',$this->plugin_slug); ?></label>
-			</th>
-			<td>
-				<input type="checkbox" {{#if multi_price}} checked="checked" {{/if}} name="multi_price" />
-				<small><?php _e('Allowing you to add specific prices per active currency.',$this->plugin_slug); ?></small>
-			</td>
-		</tr>	
-		<tr class="form-field banner-wrap">
-			<th class="table_heading" style="padding-bottom:0px;" scope="row" colspan="2">
-			<label><h3 style="margin-bottom:0px;"><?php _e('Currency Switcher',$this->plugin_slug); ?></h3></label>			
-			</th>
-		</tr>	
-
-		<tr class="form-field-wrap">
-			<th scope="row">
-				<label for="currency_menu_switcher"><?php _e('Display in Menu',$this->plugin_slug); ?></label>
-			</th>
-			<td><ul>
-			<?php 	
-			$all_menus = get_registered_nav_menus();
-			if(is_array($all_menus) && !empty($all_menus)){
-				foreach($all_menus as $slug => $label){
-					$checked = $hidden = '';
-					if(is_array($this->menus) && array_key_exists($slug,$this->menus)){
-						$checked='checked="checked"';
-					}
-					?>
-					<li>
-						<input type="checkbox" <?php echo $checked; ?> name="currency_menu_switcher[<?php echo $slug; ?>]" /> <label for="additional_currencies"><?php echo $label; ?></label> 
-					</li>
-				<?php }
-			}else{
-				echo '<li><p>'.__('You have no menus set up.',$this->plugin_slug).'</p></li>';
-			}
-			?>
-			</ul></td>
-		</tr>
-
-		<tr class="form-field">
-			<th scope="row">
-				<label for="display_flags"><?php _e('Display Flags',$this->plugin_slug); ?></label>
-			</th>
-			<td>
-				<input type="checkbox" {{#if display_flags}} checked="checked" {{/if}} name="display_flags" />
-				<small><?php _e('Displays a small flag in front of the name.',$this->plugin_slug); ?></small>
-			</td>
-		</tr>
-		<tr class="form-field">
-			<th scope="row">
-				<label for="flag_position"><?php _e('Flag Position',$this->plugin_slug); ?></label>
-			</th>
-			<td>
-				<input type="checkbox" {{#if flag_position}} checked="checked" {{/if}} name="flag_position" />
-				<small><?php _e('This moves the flag to the right (after the symbol).',$this->plugin_slug); ?></small>
-			</td>
-		</tr>				
-
-		<tr class="form-field">
-			<th scope="row">
-				<label for="currency_switcher_position"><?php _e('Symbol Position',$this->plugin_slug); ?></label>
-			</th>
-			<td>
-				<input type="checkbox" {{#if currency_switcher_position}} checked="checked" {{/if}} name="currency_switcher_position" />
-				<small><?php _e('This moves the symbol for the switcher to the left (before the flag).',$this->plugin_slug); ?></small>
-			</td>
-		</tr>
+		?>	
+			<tr class="form-field">
+				<th scope="row" colspan="2"><label><h3>{plugin-label}</h3></label></th>
+			</tr>	
+			<tr class="form-field">
+				<th scope="row">
+					<label for="text"><?php _e('Text Input','lsx-tour-operators'); ?></label>
+				</th>
+				<td>
+					<input type="text" {{#if text}} value="{{text}}" {{/if}} name="text" />
+				</td>
+			</tr>				
+			<tr class="form-field">
+				<th scope="row">
+					<label for="checkbox"><?php _e('Checkbox','lsx-tour-operators'); ?></label>
+				</th>
+				<td>
+					<input type="checkbox" {{#if checkbox}} checked="checked" {{/if}} name="checkbox" />
+					<small><?php _e('An example of a checkbox',$this->plugin_slug); ?></small>
+				</td>
+			</tr>				
 		<?php	
-	}
-
-	/**
-	 * outputs the dashboard tabs settings scripts
-	 */
-	public function settings_scripts() {
-		?>
-		<script>
-			var LSX_Select_Checkbox = {
-				initThis: function() {
-					if('undefined' != jQuery('.lsx-select-trigger') && 'undefined' != jQuery('.lsx-checkbox-action') ){
-						this.watchSelect();
-						//this.watchCheckbox();
-					}
-				},
-				watchSelect: function() {
-					jQuery('.lsx-select-trigger select').change(function(event){
-						event.preventDefault();
-						var name = jQuery(this).attr('name');
-						var value = jQuery(this).val();
-						jQuery('[data-trigger="'+name+'"] li.hidden input[checked="checked"]').removeAttr("checked").parents('li').show().removeClass('hidden');
-						jQuery('[data-trigger="'+name+'"] input[name="additional_currencies['+value+']"]').attr('checked','checked').parents('li').hide().addClass('hidden');
-					});
-				},
-				watchCheckbox: function() {
-					jQuery('.lsx-checkbox-action input').change(function(event){
-						event.preventDefault();
-						var name = jQuery(this).attr('data-name');
-						var value = jQuery(this).attr('data-value');
-						console.log(value);
-
-						jQuery('[data-trigger="'+name+'"] option[selected="selected"]').removeAttr('selected');
-						jQuery('[data-trigger="'+name+'"] option[value="'+value+'"]').attr('selected','selected');
-					});					
-				}
-			};	
-			jQuery(document).ready(function() {
-				LSX_Select_Checkbox.initThis();
-			});
-		</script>
-		<?php
-	}	
-
-	/**
-	 * outputs the dashboard tabs settings
-	 */
-	public function fields($field) {
-		if(true === $this->multi_prices && !empty($this->additional_currencies)){
-			$currency_options = array();
-			foreach($this->additional_currencies as $key => $values){
-				if($key === $this->base_currency){continue;}
-				$currency_options[$key] = $this->available_currencies[$key];
-			}
-
-			return array(
-				array( 'id' => 'price_title',  'name' => __('Prices',$this->plugin_slug), 'type' => 'title' ),
-				array( 'id' => 'price',  'name' => 'Base Price ('.$this->base_currency.')', 'type' => 'text' ),
-				array(
-						'id' => 'additional_prices',
-						'name' => '',
-						'single_name' => 'Price',
-						'type' => 'group',
-						'repeatable' => true,
-						'sortable' => true,
-						'fields' => array(
-								array( 'id' => 'amount',  'name' => 'Amount', 'type' => 'text' ),
-								array( 'id' => 'currency', 'name' => 'Currency', 'type' => 'select', 'options' => $currency_options ),
-						)
-				)			
-			);	
-		}else{
-			return array(array( 'id' => 'price',  'name' => 'Price ('.$this->base_currency.')', 'type' => 'text' ));
-		}	
 	}	
 }
-new LSX_Currency_Admin();
+new {class-name}_Admin();
--- classes/class-{plugin-name}-frontend.php
+++ classes/class-{plugin-name}-frontend.php
@@ -1,49 +1,16 @@
 <?php
 /**
- * LSX Currency Frontend Main Class
+ * {class-name} Frontend Main Class
  */
-class LSX_Currency_Frontend extends LSX_Currency{	
 
-	/** @var obj  */
-	public $rates = false;
-
-	/** @var string  */
-	public $current_currency = false;
+class {class-name}_Frontend extends {class-name}{
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
-		$this->set_defaults();
-		if(false !== $this->app_id){
-			add_filter('lsx_custom_field_query',array($this,'price_filter'),20,5);
-			add_action( 'wp_enqueue_scripts', array( $this, 'assets' ) );
-			add_filter( 'wp_nav_menu_items', array( $this, 'wp_nav_menu_items_filter' ), 10, 2 );
-		}
-	}
-
-	/**
-	 * Constructor
-	 */
-	public function set_defaults() {
-		parent::set_defaults();
-		if(false !== $this->app_id && '' !== $this->app_id){
-			//if ( false === ( $this->rates = get_transient( 'lsx_currency_rates' ) ) ) {
-				$rates = wp_remote_retrieve_body( wp_safe_remote_get( 'http://openexchangerates.org/api/latest.json?app_id=' . $this->app_id ) );
-				$decoded_rates = json_decode( $rates );	
-
-				if ( is_wp_error( $rates ) || ! empty( $decoded_rates->error ) || empty( $rates ) ) {
-					if ( 401 == $decoded_rates->status ) {
-						$this->message = __('Your API key is incorrect.',$this->plugin_slug);
-					}
-				} else {
-					set_transient( 'lsx_currency_rates', $decoded_rates->rates, 60 * 60 * 2 );
-					$this->rates = $decoded_rates->rates;
-				}
-			//}	
-		}
-		$this->current_currency = isset( $_COOKIE['lsx_currency_choice'] ) ? $_COOKIE['lsx_currency_choice'] : $this->base_currency;	
-	}					
+		add_action( 'wp_enqueue_scripts', array( $this, 'assets' ) );
+	}				
 
 	/**
 	 * Enques the assets
@@ -55,181 +22,14 @@ class LSX_Currency_Frontend extends LSX_Currency{
 		}else{
 			$min = '.min';
 		}
-
-		wp_enqueue_script( 'lsx-moneyjs', LSX_CURRENCY_URL.'/assets/js/money'.$min.'.js' , array( 'jquery' ), '0.2.0', true );
-		wp_enqueue_script( 'lsx-accountingjs', LSX_CURRENCY_URL.'/assets/js/accounting'.$min.'.js', array( 'jquery' ), '0.4.1', true );
-		wp_enqueue_script( 'lsx-jquery-cookie', LSX_CURRENCY_URL.'/assets/js/cookie'.$min.'.js', array( 'jquery' ), '2.1.3', true );
-		wp_enqueue_script( 'lsx_currency', LSX_CURRENCY_URL.'/assets/js/lsx-currency'.$min.'.js', array(
+		wp_enqueue_script( '{prefix_small}', LSX_CURRENCY_URL.'/assets/js/{plugin-name}'.$min.'.js', array(
 			'jquery',
-			'lsx-moneyjs',
-			'lsx-accountingjs',
-			'lsx-jquery-cookie'
 		), '1.0.0', true );
 
-		$params = apply_filters( 'lsx_currency_js_params', array(
-			'current_currency'       => $this->current_currency,
-			'rates'                  => $this->rates,
-			'base'                   => $this->base_currency,
-			'flags'					 => $this->display_flags,
+		$params = apply_filters( '{prefix_small}_js_params', array(
+			'ajax_url'		=>		admin_url('admin-ajax.php'),
 		));
-		wp_localize_script( 'lsx_currency', 'lsx_currency_params', $params );		
-	}
-
-	/**
-	 * Adds in the required currency conversion tags
-	 */
-	public function price_filter($return_html,$meta_key,$value,$before,$after) {
-
-		if('price' === $meta_key){
-
-			$additional_html = '';
-			$additional_prices = get_post_meta(get_the_ID(),'additional_prices',false);
-			$prefix = '<span class="amount lsx-currency" ';
-
-			if(true === $this->multi_prices && !empty($additional_prices)){
-				foreach($additional_prices as $a_price){
-					$additional_html .= ' data-price-'.$a_price['currency'].'="'.$a_price['amount'].'"';
-				}
-			}
-
-			$value = preg_replace('/[^0-9.]+/', '', $value);
-			$decimals = substr_count($value, '.');
-			if(false !== $decimals && $decimals > 1){
-				$decimals--;
-				$decimals = (int)$decimals;
-				$value = preg_replace('/'.preg_quote('.', '/').'/', '', $value, $decimals);
-			}
-
-			$prefix .= '>';
-			$suffix = '</span>';
-
-			setlocale(LC_MONETARY, 'en_US');
-
-			//work out the other tags
-			$currency = '<span class="currency-icon '. mb_strtolower( $this->base_currency ) .'">'. $this->base_currency .'</span>';
-			$amount = '<span class="value" data-price-'.$this->base_currency.'="'.$value.'" '.$additional_html.'>'.str_replace('USD','',money_format('%i',ltrim(rtrim($value)))).'</span>';
-
-			//Check for a price type and add that in.
-			$price_type = get_post_meta(get_the_ID(),'price_type',true);
-			switch($price_type){
-
-				case 'per_person_per_night':
-				case 'per_person_sharing':
-				case 'per_person_sharing_per_night':
-					$amount = $currency.$amount.' '.ucwords(str_replace('_',' ',$price_type));
-				break;
-
-				case 'total_percentage':
-					$amount .= '% '.__('Off',$this->plugin_slug);
-					$before = str_replace('from', '', $before);
-				break;
-
-				case 'none':
-				default:
-					$amount = $currency.$amount;
-				break;
-			}
-
-			$return_html = $before.$prefix.$amount.$suffix.$after;
-		}
-		return $return_html;
-	}	
-
-	/**
-	 * Filter on the 'wp_nav_menu_items' hook, that potentially adds a currency switcher to the item of some menus.
-	 *
-	 * @param string $items
-	 * @param object $args
-	 *
-	 * @return string
-	 */
-	function wp_nav_menu_items_filter( $items, $args ) {
-		if ( false !== $this->options['general'] && false !== $this->menus && array_key_exists($args->theme_location,$this->menus) ) {
-			$items .= $this->get_menu_html( $args );
-		}
-		return $items;
-	}
-
-	/**
-	 * Returns the HTML string of the language switcher for a given menu.
-	 *
-	 * @param object $args
-	 *
-	 * @return string
-	 */
-	private function get_menu_html( $args ) {
-
-		if ( empty( $this->additional_currencies ) ) {
-			return '';
-		}
-
-		$items = '';
-		$items .= '<li class="menu-item menu-item-currency menu-item-currency-current menu-item-has-children dropdown">';
-		$items .= isset( $args->before ) ? $args->before : '';
-		$items .= '<a class="current symbol-'.$this->switcher_symbol_position.'" href="#'.strtolower($this->current_currency).'">';
-		$items .= isset( $args->link_before ) ? $args->link_before : '';
-
-		if(true === $this->display_flags && 'left' === $this->flag_position){$items .= $this->get_currency_flag($this->current_currency);}
-		if('left' === $this->switcher_symbol_position){ $items .= '<span class="currency-icon '.strtolower($this->current_currency).'"></span>'; }
-
-		$items .= $this->current_currency;
-
-		if('right' === $this->switcher_symbol_position){ $items .= '<span class="currency-icon '.strtolower($this->current_currency).'"></span>'; }
-		if(true === $this->display_flags && 'right' === $this->flag_position){$items .= $this->get_currency_flag($this->current_currency);}
-
-		$items .= isset( $args->link_after ) ? $args->link_after : '';
-		$items .= '<span class="caret"></span></a>';
-		$items .= isset( $args->after ) ? $args->after : '';
-		//unset( $languages[ $current_language ] );
-		//
-		$items .= $this->render_sub_items();
-		$items .= '</li>';
-		return $items;
-	}
-	/**
-	 * Returns the HTML string of the language switcher for a given menu.
-	 *
-	 * @param object $args
-	 *
-	 * @return string
-	 */
-	private function render_sub_items() {
-
-		$sub_items = '';
-		foreach ( $this->additional_currencies as $key => $currency ) {
-			$hidden = '';
-			$class='';
-			if($this->current_currency === $key){
-				$hidden='style="display:none";';
-				$class='hidden';
-			}
-
-			$sub_items .= '<li '.$hidden.' class="'.$class.' menu-item menu-item-currency '.$this->switcher_symbol_position.'">';
-
-			$sub_items .= '<a class=" symbol-'.$this->switcher_symbol_position.'" href="#'.strtolower($key).'">';
-			if(true === $this->display_flags && 'left' === $this->flag_position){$sub_items .= $this->get_currency_flag($key);}
-			if('left' === $this->switcher_symbol_position){ $sub_items .= '<span class="currency-icon '.strtolower($key).'"></span>'; }		
-			$sub_items .= ucwords($key);
-			if('right' === $this->switcher_symbol_position){ $sub_items .= '<span class="currency-icon '.strtolower($key).'"></span>'; }
-			if(true === $this->display_flags && 'right' === $this->flag_position){$sub_items .= $this->get_currency_flag($key);}
-			$sub_items .= '</a></li>';
-		}
-
-		$sub_items = '<ul class="sub-menu submenu-currency dropdown-menu">' . $sub_items . '</ul>';
-		return $sub_items;		
-	}
-
-	/**
-	 * Returns Currency Flag for currency code provided
-	 *
-	 * @param string $key
-	 *
-	 * @return string
-	 */
-	public function get_currency_flag($key='USD') {
-		if(true === $this->display_flags){
-			return '<span class="flag-icon flag-icon-'.$this->flag_relations[$key].'"></span> ';
-		}
+		wp_localize_script( '{prefix_small}', '{prefix_small}_params', $params );		
 	}
 }
-new LSX_Currency_Frontend();
+new {class-name}_Frontend();
