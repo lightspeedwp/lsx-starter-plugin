c0a3033e6f740788288d35750aabc923a24d31f4
Warwick Booth <warwick@lsdev.biz>
2019-05-13 15:43:14 +0200

    Added in the archive, single and taxonomy templates.

--- templates/taxonomy-term.php
+++ templates/taxonomy-term.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The Template for displaying all single posts.
+ * The template for displaying Taxonomy archives
  *
  * @package lsx-starter-plugin
  */
@@ -19,11 +19,21 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="post-wrapper">
 
-				<?php include( LSX_STARTER_PLUGIN_PATH . '/templates/content-custom-post-type.php' ); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; ?>
+					<?php include( LSX_STARTER_PLUGIN_PATH . '/templates/content-custom-post-type.php' ); ?>
+
+				<?php endwhile; ?>
+
+			</div>
+
+			<?php lsx_paging_nav(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'partials/content', 'none' ); ?>
 
 		<?php endif; ?>
 
@@ -33,22 +43,11 @@ get_header(); ?>
 
 	<?php lsx_content_after(); ?>
 
-	<?php
-		if ( is_singular( 'post' ) ) {
-			lsx_post_nav();
-		}
-	?>
-
-	<?php
-		if ( comments_open() ) {
-			comments_template();
-		}
-	?>
-
 </div><!-- #primary -->
 
 <?php lsx_content_wrap_after(); ?>
 
 <?php get_sidebar(); ?>
 
-<?php get_footer();
+<?php
+get_footer();
